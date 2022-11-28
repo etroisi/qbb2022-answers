7775dd1f5f2886d5e019d2b2470884067d33a743
Emma <etroisi2@jh.edu>
2022-11-28 18:50:20 -0500

    adding things

--- week4-homework/manhattangsscript.py
+++ week4-homework/manhattangsscript.py
@@ -41,6 +41,59 @@ ax.scatter(range(len(neglogpval)), neglogpval, c="royalblue")
 ax.scatter(pvalsigxcoord, neglogsigpval, c="lightcoral")
 ax.set_xlabel("SNP")
 ax.set_ylabel("-log(pvalue)")
-plt.show()
+#plt.show()
 
-print(pvalsigxcoord)
+f=open("gs451.assoc.linear")
+
+for index, line in enumerate(f):
+    if index == 0:#skip the header
+        continue
+    fields = line.rstrip().split()#make into a list you can grab info from
+    fields[8]=float(fields[8])#make the p values into floats
+    if index == 1:
+        lowestpvalue = fields[8]
+    if fields[8] < lowestpvalue:
+        lowestpvalue = fields[8]
+        snpid = fields[1]
+        
+f=open("/Users/cmdb/qbb2022-answers/week4-homework/gwas_data/genotypes.vcf")
+for index, line in enumerate(f):
+    if line.strip('"').startswith("##"):
+        continue
+    fields = line.strip('"\r\n').split('\t')
+    snp = fields[2]
+    if snp==snpid:
+        importantsnp=fields
+
+print(len(importantsnp))
+f=open("/Users/cmdb/qbb2022-answers/week4-homework/gwas_data/GS451_IC50.txt")
+pheno00=[]
+pheno01=[]
+pheno11=[]
+for index, line in enumerate(f):
+    if index == 0:
+        continue
+    fields = line.strip('"\r\n').split('\t')
+    if fields[2] == "NA":
+        continue
+    ic50=float(fields[2])
+    if importantsnp[index+8]=="0/0":
+        pheno00.append(ic50)
+    if importantsnp[index+8]=="0/1":
+        pheno01.append(ic50)
+    if importantsnp[index+8]=="1/0":
+        pheno01.append(ic50)
+    if importantsnp[index+8]=="1/1":
+        pheno11.append(ic50)
+
+print(pheno00)
+print(pheno01)
+print(pheno11)
+
+fig, ax = plt.subplots()
+plt.boxplot([pheno00, pheno01, pheno11])
+ax.set_xlabel("genotype")
+ax.set_ylabel("effect")
+ax.set_title("GS IC50")
+plt.show()
+#print(pvalsigxcoord)
