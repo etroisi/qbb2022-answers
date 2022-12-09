96600b431a9e32eb89e45a3de633d21840c8ba07
Emma <etroisi2@jh.edu>
2022-12-09 16:10:36 -0500

    adding script.py

--- week11-homework/script.py
+++ week11-homework/script.py
@@ -30,4 +30,28 @@ sc.tl.rank_genes_groups(adata, groupby='leiden', method='t-test')
 
 #rank using logistic regression
 sc.tl.rank_genes_groups(adata, groupby='leiden', method='logreg')
-sc.pl.rank_genes_groups(adata)
+#sc.pl.rank_genes_groups(adata)
+
+#AC cells are cluster 5, Dpi and Fabp3
+#MG cells are cluster 26, fcgr3 and c1qb
+#OL cells are cluster 21, olig1 and 2
+#aSMC cluster 4, slit3
+#vEC cluster 16, hbb-bs
+#aEC cluster 9, igfbp3
+
+clusternames = {
+    '5': 'AC',
+    '26': 'MG',
+    '21': 'OL',
+    '4': 'aSMC',
+    '16': 'vEC',
+    '9': 'aEC'
+}
+adata.obs['cell_type'] = adata.obs['leiden'].map(clusternames).astype('category')
+sc.pl.tsne(adata, color = 'cell_type')
+sc.pl.tsne(adata, color = 'Dbi')
+sc.pl.tsne(adata, color = 'Fcgr3')
+sc.pl.tsne(adata, color = 'Olig1')
+sc.pl.tsne(adata, color = 'Slit3')
+sc.pl.tsne(adata, color = 'Hbb-bs')
+sc.pl.tsne(adata, color = 'Igfbp3')
